f2b9cfce03792eb49fd995d5b736ee5f0690bf72
sharadika <madu.sharadika@gmail.com>
2022-11-23 00:37:38 +0530

    Repository Unit Tests

--- UnitTest/RepositoryTest/TestEmployeeRepository.cs
+++ UnitTest/RepositoryTest/TestEmployeeRepository.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Common.Dto;
+using Data;
 using Data.Models;
 using Data.Repository;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +13,167 @@ namespace UnitTest.RepositoryTest
 {
     public class TestEmployeeRepository
     {
+        public readonly DbContextOptions<EmployeeContext> _dbOptions;
+        private readonly IMapper _mapper;
+        private EmployeeContext _empContext;
+        private Repository _repository;
+        public TestEmployeeRepository()
+        {
+            _dbOptions = new DbContextOptionsBuilder<EmployeeContext>()
+                            .UseInMemoryDatabase(databaseName: "EmployeeDb")
+                            .Options;
 
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            _mapper = mockMapper.CreateMapper();
+        }
+
+        public void AddDataToDBContext()
+        {
+            _empContext = new EmployeeContext(_dbOptions);
+            _repository = new Repository(_empContext, _mapper);
+
+            for (int i = 1; i < 4; i++)
+            {
+                var newEmp = new Employee()
+                {
+                    Id = i,
+                    CreatedBy = 1001,
+                    CreatedOn = DateTime.Parse("2022-04-05 00:00:00.0000000"),
+                    UpdatedBy = 1002,
+                    UpdatedOn = DateTime.Parse("2022-04-05 00:00:00.0000000"),
+                    FirstName = "Micheal" + i,
+                    LastName = "Bruce",
+                    City = "Rome",
+                    ContactNo = "0716163291",
+                    IsDeleted = false,
+                    DepartmentId = 1
+                };
+
+                _empContext.Employees.Add(newEmp);
+                _empContext.SaveChanges();
+            }
+
+            var newDep = new Department()
+            {
+                Id = 1,
+                CreatedBy = 1001,
+                CreatedOn = DateTime.Parse("2022-04-05 00:00:00.0000000"),
+                UpdatedBy = 1002,
+                UpdatedOn = DateTime.Parse("2022-04-05 00:00:00.0000000"),
+                IsDeleted = false,
+                DepartmentName = "IT"
+            };
+            _empContext.Departments.Add(newDep);
+            _empContext.SaveChanges();
+        }
+
+        [Fact()]
+        public void Create_Employee()
+        {
+            // Arrange
+            var EmpContext = new EmployeeContext(_dbOptions);
+
+            Repository repository = new Repository(EmpContext, _mapper);
+            var newEmp = new EmployeeDto()
+            {
+                Id = 50,
+                CreatedBy = 1001,
+                CreatedOn = DateTime.Parse("2022-04-05 00:00:00.0000000"),
+                UpdatedBy = 1002,
+                UpdatedOn = DateTime.Parse("2022-04-05 00:00:00.0000000"),
+                FirstName = "Micheal",
+                LastName = "Bruce",
+                City = "Rome",
+                ContactNo = "0716163291",
+                IsDeleted = false,
+                DepartmentId = 1
+            };
+
+            // Act
+            var result = repository.CreateEmployee(newEmp);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact()]
+        public void Return_Employee_List()
+        {
+            // Arrange
+            AddDataToDBContext();
+
+            // Act
+            var result = _repository.GetEmployees();
+
+            // Assert
+            Assert.True(result.Count > 0);
+        }
+
+        [Fact()]
+        public void Return_EmployeeTest_When_Id_Exixts()
+        {
+            // Arrange
+            AddDataToDBContext();
+
+            // Act
+            var result = _repository.GetEmployee(1);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact()]
+        public void Delete_EmployeeTest_When_Id_Exixts()
+        {
+            // Arrange
+            AddDataToDBContext();
+
+            // Act
+            var result = _repository.DeleteEmployee(1);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact()]
+        public void Update_Employee_When_Id_Exixts()
+        {
+            // Arrange
+            AddDataToDBContext();
+
+            var newEmp = new EmployeeDto()
+            {
+                Id = 1,
+                UpdatedBy = 1002,
+                UpdatedOn = DateTime.Parse("2022-04-05 00:00:00.0000000"),
+                FirstName = "Micheal_New",
+                LastName = "Bruce_New",
+                City = "Rome_New",
+                ContactNo = "0716163292",
+                DepartmentId = 2
+            };
+
+            // Act
+            var result = _repository.UpdateEmployee(newEmp);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void GetDepartmentByEmployeeId__When_EmpId_Exixts()
+        {
+            //Arrange
+            AddDataToDBContext();
+
+            // Act
+            var result = _repository.GetDepartmentByEmployeeId(1);
+
+            // Assert
+            Assert.NotNull(result);
+        }
     }
 }
